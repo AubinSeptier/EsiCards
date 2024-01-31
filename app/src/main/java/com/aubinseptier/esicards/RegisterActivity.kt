b0bdb74ebd57621c8aec42a7f841f394ecb4ab5c
Aubin Septier <aubinseptier@gmail.com>
2024-01-31 16:07:50 +0100

    Design Register layout and add from login to register action (and vice versa)

--- app/src/main/java/com/aubinseptier/esicards/RegisterActivity.kt
+++ app/src/main/java/com/aubinseptier/esicards/RegisterActivity.kt
@@ -1,23 +1,22 @@
 package com.aubinseptier.esicards
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 
-    public fun registerNewAccount(view: View){
-       val intent = Intent(this, RegisterActivity::class.java)
-       startActivity(intent)
+    public fun goToLogin(view: View){
+        finish()
     }
 }
 
-data class LoginData(
+data class RegisterData(
+    val name: String,
     val mail: String,
     val password: String
 )

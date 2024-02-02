8dd9c77310d27beec5c43ca67899adcce8071705
Aubin Septier <aubinseptier@gmail.com>
2024-02-02 11:45:27 +0100

    Add login functionnality

--- app/src/main/java/com/aubinseptier/esicards/MainActivity.kt
+++ app/src/main/java/com/aubinseptier/esicards/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +12,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    public fun login(view: View){
+        var loginEmail = findViewById<EditText>(R.id.loginEmail)
+        var loginPassword = findViewById<EditText>(R.id.loginPassword)
+        var logData = LoginData(loginEmail.text.toString(), loginPassword.text.toString())
+        Api().post<LoginData, String>("https://esicards.lesmoulinsdudev.com/auth", logData, ::LoginSuccess)
+    }
+
     public fun registerNewAccount(view: View){
        val intent = Intent(this, RegisterActivity::class.java)
        startActivity(intent)
+    }
+
+    private fun LoginSuccess(responseCode: Int, token: String?){
+        if(responseCode == 200){
+            val intent = Intent(this, CardsListActivity::class.java)
+            intent.putExtra("token", token)
+            startActivity(intent)
+        }
     }
 }
 
